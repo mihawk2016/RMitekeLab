965e4d6985c349ef66bf75b17d622c389b666914
Zhang <z.zhang@miteke.com>
2017-02-07 16:57:32 +0800

    parallel

--- R/hello.R
+++ R/hello.R
@@ -3,26 +3,33 @@
 library(compiler)
 compilePKGS(T)
 
-read.mq.file <- function(mq.files) {
-  # ''' read mq files '''
+read.mq.file <- function(mq.files, cl) {
+  # ''' read mq files (V) '''
   # @param mq.files: MetaQuote files.
   # @return:
   # 2017-02-05: Version 0.1
   mq.names <- mq.file.name(mq.files)
-  fetch.file.data(mq.files, mq.names)
+  if (missing(cl) || length(mq.files) < 4) {
+    mapply(fetch.file.data, mq.files, mq.names)
+  } else (
+    clusterMap(cl, fetch.file.data, mq.files, mq.names)
+  )
+  
+  # fetch.file.data(mq.files, mq.names)
 }
 
 mq.file.name <- function(mq.files) {
   # ''' get mq file names (V) '''
   # @param mq.files: MetaQuote files.
   # @return: names of MetaQuote files.
+  # 2017-02-07: Version 1.0
   # 2017-02-06: Version 0.2 ifelse not good for this. is.data.frame or is.character return 1-length vector.
   # 2017-02-05: Version 0.1
   if (is.data.frame(mq.files)) {
     return(mq.files$name)
   }
   return(basename(mq.files))
-}
+} # FINISH
 
 fetch.file.data <- function(mq.file, mq.file.name) {
   # ''' fetch mq file's data (S) '''
@@ -46,10 +53,6 @@ fetch.file.data <- function(mq.file, mq.file.name) {
   ## ToDo ####
 }
 
-# fetch.html.data.old <- function(mq.file, mq.file.name) {
-#   html.parse <- htmlParse(mq.file, encoding = 'UTF-8')
-#   xmlValue(getNodeSet(html.parse,'//title')[[1]])
-# }
 
 fetch.html.data <- function(mq.file) {
   # ''' fetch mq html's data (S) '''
@@ -57,7 +60,6 @@ fetch.html.data <- function(mq.file) {
   # @param mq.names: MetaQuote file-name.
   # @return: data of MetaQuote file.
   # 2017-02-05: Version 0.1
-  library(xml2)
   parse <- tryCatch(
     read_html(mq.file, encoding = 'GBK'),
     error = function(e) read_html(mq.file, encoding = 'UTF-8')
@@ -66,28 +68,32 @@ fetch.html.data <- function(mq.file) {
   # infos <-
   if (grepl('Strategy Tester:', title)) {
     infos <- fetch.html.data.infos.mt4ea(parse)
+    tickets <- fetch.html.data.tickets.mt4ea(mq.file)
     # return(MetaQuote.HTML.MT4EA.Report$new(file.path, file.name, html.parse))
   } else if (grepl('Statement:', title)) {
     infos <- fetch.html.data.infos.mt4trade(parse)
+    tickets <- fetch.html.data.tickets.mt4trade(mq.file)
     # return(MetaQuote.HTML.MT4Trade.Report$new(file.path, file.name, html.parse))
   } else if (grepl('Strategy Tester Report', title)) {
     infos <- fetch.html.data.infos.mt5ea(parse)
+    tickets <- fetch.html.data.tickets.mt5ea(mq.file)
     # return(MetaQuote.HTML.MT5EA.Report$new(file.path, file.name, html.parse))
   } else if (grepl('Trade History Report', title)) {
     infos <- fetch.html.data.infos.mt5trade(parse)
+    tickets <- fetch.html.data.tickets.mt5trade(mq.file)
     # return(MetaQuote.HTML.MT5Trade.Report$new(file.path, file.name, html.parse))
   } else if (grepl('Closed Trades Report', title)) {
     infos <- fetch.html.data.infos.mt4m_closed(parse)
+    tickets <- fetch.html.data.tickets.mt4m_closed(mq.file)
     # return(MetaQuote.HTML.MT4M_Closed.Report$new(file.path, file.name))
   } else if (grepl('Raw Report', title)) {
     infos <- fetch.html.data.infos.mt4m_raw(parse)
+    tickets <- fetch.html.data.tickets.mt4m_raw(mq.file)
     # return(MetaQuote.HTML.MT4M_Raw.Report$new(file.path, file.name))
   }
   # return(NULL)
 }
 
-
-#### FETCH INFOS ####
 fetch.html.data.infos.mt4ea <- function(mq.file.parse) {
   
   head.lines <- xml_text(xml_find_all(mq.file.parse, '//b')[2:3])
@@ -282,11 +288,17 @@ format.time.numeric.to.posixct <- function(time) {
   as.POSIXct(time, origin = '1970-01-01', tz = 'GMT')
 }
 
+
 #### FETCH TICKETS ####
-fetch.html.data.tickets.mt4ea <- function(mq.file.parse) {
+fetch.html.data.tickets.mt4ea <- function(mq.file) {
+  table <- readHTMLTable(mq.file, stringsAsFactors = FALSE, encoding = 'GBK', which = 2)
+  # 
+  # table.values <- xml_text(xml_find_all(xml_find_all(xml_find_all(mq.file.parse, '//table')[2], './/tr'), './/td'))
+  # colspan <- as.numeric(xml_attr(xml_find_all(xml_find_all(mq.file.parse, '//table')[2], './/td'), 'colspan'))
+  # print(xml_length(xml_find_all(xml_find_all(mq.file.parse, '//table'), './/tr'), only_elements = FALSE))
+  # build.html.table(table.values, colspan, 10)
   
-  table <- xml_find_all(mq.file.parse, '//table')[2]
-  
+  # data.ta
   # head.lines <- xml_text(xml_find_all(mq.file.parse, '//table')[2])
   # first.table <- xml_find_first(mq.file.parse, '//table')
   # time.string <- xml_text(xml_find_all(first.table, '//td')[4])
@@ -299,68 +311,65 @@ fetch.html.data.tickets.mt4ea <- function(mq.file.parse) {
   # )
 }
 
-# fetch.html.data.tickets.mt4trade <- function(mq.file.parse) {
-#   
-#   first.row <- xml_text(xml_find_all(xml_find_first(xml_find_first(mq.file.parse, '//table'), './/tr'), './/b'))
-#   build.infos(
-#     type = 'MT4-Trade',
-#     account = first.row[grep('Account', first.row)],
-#     name = first.row[grep('Name', first.row)],
-#     broker = xml_text(xml_find_first(mq.file.parse, '//b')),
-#     currency = first.row[grep('Currency', first.row)],
-#     leverage = first.row[grep('Leverage', first.row)],
-#     time = tail(first.row, 1)
-#   )
-# }
-# 
-# fetch.html.data.tickets.mt5ea <- function(mq.file.parse) {
-#   
-#   table.values <- xml_text(xml_find_all(xml_find_first(mq.file.parse, '//table'), './/td'))
-#   time.string <- table.values[grep('Period:', table.values) + 1]
-#   nchar.time.string <- nchar(time.string)
-#   build.infos(
-#     type = 'MT5-EA',
-#     name = table.values[grep('Expert:', table.values) + 1],
-#     broker = table.values[grep('Broker:', table.values) + 1],
-#     currency = table.values[grep('Currency:', table.values) + 1],
-#     leverage = table.values[grep('Leverage:', table.values) + 1],
-#     time = substr(time.string, nchar.time.string - 10, nchar.time.string - 1)
-#   )
-# }
-# 
-# fetch.html.data.tickets.mt5trade <- function(mq.file.parse) {
-#   
-#   table.values <- xml_text(xml_find_all(xml_find_first(mq.file.parse, '//table'), './/th'))
-#   account.currency.leverage <- table.values[grep('Account:', table.values) + 1]
-#   build.infos(
-#     type = 'MT5-Trade',
-#     account = account.currency.leverage,
-#     name = table.values[grep('Name:', table.values) + 1],
-#     broker = table.values[grep('Broker:', table.values) + 1],
-#     currency = account.currency.leverage,
-#     leverage = account.currency.leverage,
-#     time = format.infos.time(table.values[grep('Date:', table.values) + 1]) - 8 * 3600
-#   )
-# }
-# 
-# fetch.html.data.tickets.mt4m_closed <- function(mq.file.parse) {
-#   build.infos(
-#     type = 'MT4M-Closed'
-#   )
-# }
-# 
-# fetch.html.data.tickets.mt4m_raw <- function(mq.file.parse) {
-#   build.infos(
-#     type = 'MT4M-Raw'
-#   )
-# }
+fetch.html.data.tickets.mt4trade <- function(mq.file) {
+  
+  table <- readHTMLTable(mq.file, stringsAsFactors = FALSE, encoding = 'UTF-8', which = 1)
+  # first.row <- xml_text(xml_find_all(xml_find_first(xml_find_first(mq.file.parse, '//table'), './/tr'), './/b'))
+  # build.infos(
+  #   type = 'MT4-Trade',
+  #   account = first.row[grep('Account', first.row)],
+  #   name = first.row[grep('Name', first.row)],
+  #   broker = xml_text(xml_find_first(mq.file.parse, '//b')),
+  #   currency = first.row[grep('Currency', first.row)],
+  #   leverage = first.row[grep('Leverage', first.row)],
+  #   time = tail(first.row, 1)
+  # )
+}
 
+fetch.html.data.tickets.mt5ea <- function(mq.file) {
+  
+  table <- readHTMLTable(mq.file, stringsAsFactors = FALSE, encoding = 'UTF-8', which = 2)
+  # table.values <- xml_text(xml_find_all(xml_find_first(mq.file.parse, '//table'), './/td'))
+  # time.string <- table.values[grep('Period:', table.values) + 1]
+  # nchar.time.string <- nchar(time.string)
+  # build.infos(
+  #   type = 'MT5-EA',
+  #   name = table.values[grep('Expert:', table.values) + 1],
+  #   broker = table.values[grep('Broker:', table.values) + 1],
+  #   currency = table.values[grep('Currency:', table.values) + 1],
+  #   leverage = table.values[grep('Leverage:', table.values) + 1],
+  #   time = substr(time.string, nchar.time.string - 10, nchar.time.string - 1)
+  # )
+}
 
+fetch.html.data.tickets.mt5trade <- function(mq.file) {
+  
+  table <- readHTMLTable(mq.file, stringsAsFactors = FALSE, encoding = 'UTF-8', which = 1)
+  # table.values <- xml_text(xml_find_all(xml_find_first(mq.file.parse, '//table'), './/th'))
+  # account.currency.leverage <- table.values[grep('Account:', table.values) + 1]
+  # build.infos(
+  #   type = 'MT5-Trade',
+  #   account = account.currency.leverage,
+  #   name = table.values[grep('Name:', table.values) + 1],
+  #   broker = table.values[grep('Broker:', table.values) + 1],
+  #   currency = account.currency.leverage,
+  #   leverage = account.currency.leverage,
+  #   time = format.infos.time(table.values[grep('Date:', table.values) + 1]) - 8 * 3600
+  # )
+}
 
+fetch.html.data.tickets.mt4m_closed <- function(mq.file) {
+  table <- readHTMLTable(mq.file, stringsAsFactors = FALSE, encoding = 'UTF-8', which = 1)
+  
+  # td.nodes <- xml_find_all(xml_find_all(mq.file.parse, '//table'), './/td')
+  # table.values <- xml_text(td.nodes)
+  # colspan <- as.numeric(xml_attr(td.nodes, 'colspan'))
+  # build.html.table(table.values, colspan, 17)
+}
 
-
-
-
-p.read.mq.file <- function(cl, mq.files) {
-  parLapply(cl, mq.files, read.mq.file)
+fetch.html.data.tickets.mt4m_raw <- function(mq.file) {
+  table <- readHTMLTable(mq.file, colClasses = c(rep('character', 23)), stringsAsFactors = FALSE, encoding = 'UTF-8', which = 1)
+  # build.infos(
+  #   type = 'MT4M-Raw'
+  # )
 }
